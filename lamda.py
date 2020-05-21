2c424f87fe1d4befb580870a95fbb4f4ccb61440
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-21 21:15:21 +0530

    Add files via upload

---
+++ lamda.py
@@ -0,0 +1,15 @@
+mul = lambda a: a * a                     
+print(mul(2))
+
+add = lambda a, b: a + b
+print(add(5, 5))
+
+sub = lambda a, b: a - b
+print(sub(8, 5))
+
+div = lambda a, b: a // b
+print(div(10, 2))
+
+mod = lambda a, b: a % b
+print(mod(10, 3))
+
